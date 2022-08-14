6a0495766c2caaa6cfb42c3d7621d80eba4d6c59
minhazk <minhazulkarim26@gmail.com>
2022-08-14 14:54:41 +0100

    setup

---
+++ client/src/App.js
@@ -0,0 +1,3 @@
+const App = () => 'hi';
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT);
